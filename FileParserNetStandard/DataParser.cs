6cd9e1f97133b2432fa5fd8fb38c8382d1d9a47b
102051563 <102051563@TD224-010>
2019-06-04 14:01:20 +1000

    FileParser methods completed

---
+++ FileParserNetStandard/DataParser.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FileParserNetStandard {
+    public class DataParser {
+        
+
+        /// <summary>
+        /// Strips any whitespace before and after a data value.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public List<List<string>> StripWhiteSpace(List<List<string>> data) {
+            List<List<string>> list = new List<List<string>>();
+            foreach (List<string> items in data)
+            {
+                List<string> line = new List<string>();
+                for (int o = 0; o < items.Count; o++)
+                {
+                    string item = items[o];
+                    item = item.Trim();
+                    line.Add(item);
+                }
+                list.Add(line);
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// Strips quotes from beginning and end of each data value
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public List<List<string>> StripQuotes(List<List<string>> data) {
+            List<List<string>> list = new List<List<string>>();
+            foreach (List<string> items in data)
+            {
+                List<string> line = new List<string>();
+                for (int o = 0; o < items.Count; o++)
+                {
+                    string item = items[o];
+                    item = item.Trim('"');
+                    line.Add(item);
+                }
+                list.Add(line);
+            }
+            return list;
+            
+        }
+
+    }
+}
