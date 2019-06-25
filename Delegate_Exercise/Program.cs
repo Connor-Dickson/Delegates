120425ab64a2b6c0bbad4bd0cfe4c0b9fe8edd41
102051563 <102051563@TD224-011>
2019-06-25 19:29:49 +1000

    StripHash Delegate mostly done

--- Delegate_Exercise/Program.cs
+++ Delegate_Exercise/Program.cs
@@ -10,9 +10,32 @@ namespace Delegate_Exercise {
  
     
     internal class Delegate_Exercise {
-        public static void Main(string[] args) {
-                        
- 
+
+        public static List<List<string>> StripHash(List<List<string>> data)
+        {
+            List<List<string>> list = new List<List<string>>();
+            //foreach (List<string> items in data)
+            for (int i = 0; i < data.Count; i++)
+            {
+                List<string> line = new List<string>();
+                for(int o = 0; o < i; o++)
+                {
+                    string text = line[o];
+                    text = text.Trim('#');
+                    line.Add(text);
+                }
+                list.Add(line);
+            }
+            return list;
+        }
+        public static void Main(string[] args)
+        {
+            DataParser data = new DataParser();
+            CsvHandler test = new CsvHandler();
+            Func<List<List<string>>, List<List<string>>> whiteTrimmer = new Func<List<List<string>>, List<List<string>>>(data.StripWhiteSpace);
+            whiteTrimmer += data.StripQuotes;
+            test.ProcessCsv(@"C:\Users\102051563\source\repos\Dip-Seminar-Delegates-Lambda-Linq_Exercises\Files\data.csv",
+                @"C:\Users\102051563\source\repos\Dip-Seminar-Delegates-Lambda-Linq_Exercises\Files\processed_data.csv", whiteTrimmer);
             
         }
 
